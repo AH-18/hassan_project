3f27baed31bc35181c39b997497a59d04c9d6c86
Ahmed abdullah <aa1392002@gmail.com>
2025-06-22 14:36:36 +0300

    commented ec2

--- variables.tf
+++ variables.tf
@@ -69,65 +69,66 @@ variable "public_subnets" {
 ###########################################
 ##            EC2 Variables              ##
 ###########################################
+# Commented out since EC2 module is disabled
 
-variable "ami_id" {
-  description = "The Amazon Machine Image (AMI) ID to use for the EC2 instance."
-  type        = string
-}
+# variable "ami_id" {
+#   description = "The Amazon Machine Image (AMI) ID to use for the EC2 instance."
+#   type        = string
+# }
 
-variable "instance_type" {
-  description = "The type of EC2 instance to launch (e.g., t2.micro, m5.large)."
-  type        = string
-  default     = "t2.micro"
-}
+# variable "instance_type" {
+#   description = "The type of EC2 instance to launch (e.g., t2.micro, m5.large)."
+#   type        = string
+#   default     = "t2.micro"
+# }
 
-variable "keypair_name" {
-  description = "The name of the key pair to use for SSH access to the EC2 instance."
-  type        = string
-}
+# variable "keypair_name" {
+#   description = "The name of the key pair to use for SSH access to the EC2 instance."
+#   type        = string
+# }
 
 # variable "instance_name" {
 #   description = "The name to assign to the EC2 instance for identification purposes."
 #   type        = string
 # }
 
-variable "ingress_rules" {
-  description = "List of ingress rules for EC2 security group."
-  type = list(object({
-    description     = string
-    from_port       = number
-    to_port         = number
-    protocol        = string
-    cidr_blocks     = optional(list(string), [])
-    security_groups = optional(list(string), [])
-  }))
-  default = []
-}
+# variable "ingress_rules" {
+#   description = "List of ingress rules for EC2 security group."
+#   type = list(object({
+#     description     = string
+#     from_port       = number
+#     to_port         = number
+#     protocol        = string
+#     cidr_blocks     = optional(list(string), [])
+#     security_groups = optional(list(string), [])
+#   }))
+#   default = []
+# }
 
-variable "iam_permissions" {
-  description = "List of IAM permissions or policies to attach to the EC2 instance role."
-  type        = list(string)
-  default     = []
-}
+# variable "iam_permissions" {
+#   description = "List of IAM permissions or policies to attach to the EC2 instance role."
+#   type        = list(string)
+#   default     = []
+# }
 
-variable "associate_public_ip_address" {
-  description = "Whether to associate a public IP address with the EC2 instance."
-  type        = bool
-  default     = true
-}
+# variable "associate_public_ip_address" {
+#   description = "Whether to associate a public IP address with the EC2 instance."
+#   type        = bool
+#   default     = true
+# }
 
-variable "root_block_device" {
-  description = "Configuration of the root block device for the EC2 instance."
-  type = object({
-    size                  = number
-    type                  = string
-    encrypted             = bool
-    delete_on_termination = bool
-  })
-  default = {
-    size                  = 100
-    type                  = "gp3"
-    encrypted             = true
-    delete_on_termination = true
-  }
-}
+# variable "root_block_device" {
+#   description = "Configuration of the root block device for the EC2 instance."
+#   type = object({
+#     size                  = number
+#     type                  = string
+#     encrypted             = bool
+#     delete_on_termination = bool
+#   })
+#   default = {
+#     size                  = 100
+#     type                  = "gp3"
+#     encrypted             = true
+#     delete_on_termination = true
+#   }
+# }
